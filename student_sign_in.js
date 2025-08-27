90089fd8616021e7b6d276207a2db19530b230a0
Phonic1 <scottclee635@gmail.com>
2025-08-27 13:12:58 +1000

    Delete student_sign_in.js

--- student_sign_in.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDwNqSt2UD1r0EO2CytEtqOmDiBxNIJM",
-    authDomain: "m5-literacy-db.firebaseapp.com",
-    projectId: "m5-literacy-db",
-    storageBucket: "m5-literacy-db.firebasestorage.app",
-    messagingSenderId: "389680220222",
-    appId: "1:389680220222:web:623db2ed40f30a5ad3696f",
-    measurementId: "G-PMLHCJ18F0"
-};
